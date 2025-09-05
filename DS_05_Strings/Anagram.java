58457024cd8958ac10b4534644f238670c6f448b
Harivansh Chauhan <144122932+harirockg@users.noreply.github.com>
2025-09-06 02:17:48 +0530

    added

--- DS_05_Strings/Anagram.java
+++ DS_05_Strings/Anagram.java
@@ -14,10 +14,30 @@ public class Anagram {
 
         return Arrays.equals(chr1,chr2);
     }
+
+    public static boolean checkForAnagram(String s1,String s2){
+        int count1[]=new int[26];
+        int count2[]=new int[26];
+
+        for(int i=0;i<s1.length();i++){
+            char ch=s1.charAt(i);
+            int idx=ch-'a';
+            count1[idx]++;
+        }
+
+        for(int i=0;i<s2.length();i++){
+            char ch=s2.charAt(i);
+            int idx=ch-'a';
+            count2[idx]++;
+        }
+
+        return Arrays.equals(count1,count2);
+    }
     public static void main(String[] args) {
         String s1="yash";
         String s2="sayh";
-        System.out.println(checkAnagram(s1, s2));
+        //System.out.println(checkAnagram(s1, s2));
+        System.out.println(checkForAnagram(s1, s2));
     }
     
 }
