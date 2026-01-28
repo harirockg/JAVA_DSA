85d0795d69e419f9b882ce46571921b85b5adb89
Harivansh Chauhan <144122932+harirockg@users.noreply.github.com>
2026-01-29 01:10:10 +0530

    remove cycle in LL

--- DS_14_LinkedList/LinkedList.java
+++ DS_14_LinkedList/LinkedList.java
@@ -253,6 +253,52 @@ public class LinkedList {
         return true;
     }
 
+    public static boolean isCycle() {  // floyd's cycle finding algorithm
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;  //+1
+            fast = fast.next.next;   //+2
+            if(slow == fast){
+                return true;  // cycle exists
+            }
+        }
+        return false;   // cycle doesnot exits
+    }
+
+    public static void removeCycle(){
+
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                cycle = true;
+                break;
+            }
+        }
+
+        if(cycle == false){
+            return ;
+        }
+
+        //find meeting point
+        slow = head;
+        Node prev = null;// last node
+        while(slow != fast){
+            prev = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        //remove cycle -> last.next = null
+        prev.next = null;
+
+    }
+
     public static void main(String[] args) {
 
         LinkedList ll = new LinkedList();
@@ -292,13 +338,30 @@ public class LinkedList {
         // ll.deleteNthfromEnd(3);
         // ll.print();
 
-        ll.addLast(1);
-        ll.addLast(2);
-        ll.addLast(2);
-        //ll.addLast(1);
+        // ll.addLast(1);
+        // ll.addLast(2);
+        // ll.addLast(2);
+        // //ll.addLast(1);
 
-        ll.print(); //1->2->2->1
-        System.out.println(ll.checkPalindrome());
+        // ll.print(); //1->2->2->1
+        // System.out.println(ll.checkPalindrome());
+
+
+        //detect a loop /cycle ian LL
+        head = new Node(1);
+        Node temp = new Node(2);
+        head.next = temp;
+        head.next.next = new Node(3);
+        head.next.next.next = temp;
+        //1->2->3->1
+        System.out.println(isCycle());
+
+
+        removeCycle();
+        System.out.println(isCycle());
+
+
+
 
 
 
